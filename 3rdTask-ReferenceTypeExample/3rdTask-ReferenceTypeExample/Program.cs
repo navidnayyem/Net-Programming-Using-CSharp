6754c4db7944a828ab3ba6ef25db71b3c3f9a1a0
Mohammad Navid Nayyem <44639412+navidnayyem@users.noreply.github.com>
2020-11-04 13:14:32 +0600

    Add files via upload

---
+++ 3rdTask-ReferenceTypeExample/3rdTask-ReferenceTypeExample/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _3rdTask_ReferenceTypeExample
+{
+    class Program
+    {
+        public static void qube(ref int num) 
+        { 
+            num = num * num * num; 
+        }
+        static void Main(string[] args)
+        {
+            int original = 5; 
+            Console.Write("Current Value: {0}\t", original);
+            Program.qube(ref original); 
+            Console.WriteLine("\nNow the current value is {0}\t", original); 
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 4thTask-Structure/4thTask-Structure/Structure.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+struct StudentInfo{ 
+    public int studentid; 
+    public string studentname; 
+    public string studentcontact; 
+};
+
+namespace _4thTask_Structure
+{
+    class Structure
+    {
+        static void Main(string[] args)
+        {
+            StudentInfo Student1; /* Declare Student1 */
+            StudentInfo Student2; /* Declare Student2 */
+
+            /* Student1 specification */
+            Student1.studentid = 1;
+            Student1.studentname = "Navid";
+            Student1.studentcontact = "01724463611";
+
+            /* Student2 specification */
+            Student2.studentid = 2;
+            Student2.studentname = "Nayyem";
+            Student2.studentcontact = "01614463611";
+
+            /* print Student1 info */
+            Console.WriteLine("Student1 studentid : {0}", Student1.studentid); 
+            Console.WriteLine("Student1 studentname : {0}", Student1.studentname); 
+            Console.WriteLine("Student1 studentcontact : {0}\n", Student1.studentcontact);
+            
+            /* print Student2 info */
+            Console.WriteLine("Student2 studentid : {0}", Student2.studentid);
+            Console.WriteLine("Student2 studentname : {0}", Student2.studentname);
+            Console.WriteLine("Student2 studentcontact : {0}", Student2.studentcontact);
+            Console.ReadLine();
+        }
+    }
+}
