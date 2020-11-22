b5e890fd350d27f4227312a9201b0e1c873291a6
Mohammad Navid Nayyem <44639412+navidnayyem@users.noreply.github.com>
2020-11-22 12:21:04 +0600

    Add files via upload

---
+++ Assignment 01/AssignmentTask1/AssignmentTask1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AssignmentTask1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle rect1 = new Rectangle();
+            Rectangle rect2 = new Rectangle(4,6);
+
+            //area of rectangle 1
+            rect1.setHeight(10);
+            rect1.setWidth(10);
+            rect1.showArea();
+
+            //area of rectangle 2
+            rect2.showArea();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Assignment 01/AssignmentTask1/AssignmentTask1/Rectangle.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace AssignmentTask1
+{
+    class Rectangle
+    {
+        int width, height;
+        public void setWidth(int width)
+        {
+            this.width = width;
+        }
+        public void setHeight(int height)
+        {
+            this.height = height;
+        }
+        public int getHeigth()
+        {
+            return this.height;
+        }
+        public int getWidth()
+        {
+            return this.width;
+        }
+        public void showArea()
+        {
+            Console.WriteLine("Area of Rectangle: {0}", this.width * this.height);
+        }
+    }
+}
