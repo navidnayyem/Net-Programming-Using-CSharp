751fa0d75275c037ef489c5dd5e82e4df459dd7b
Mohammad Navid Nayyem <44639412+navidnayyem@users.noreply.github.com>
2020-11-22 11:39:00 +0600

    Add files via upload

---
+++ LabTask1/LabTask1_AddTwoNumbers/LabTask1_AddTwoNumbers/AddTwoNumbers.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace LabTask1_AddTwoNumbers
+{
+    class AddTwoNumbers
+    {
+        static void Main(string[] args)
+        {
+            double num1 = Double.Parse(Console.ReadLine());
+            double num2 = Double.Parse(Console.ReadLine());
+            int n1 = Convert.ToInt32(num1);
+            int n2 = Convert.ToInt32(num2);
+            int addresult = n1 + n2;
+            Console.WriteLine("{0} + {1} = {2}", n1, n2, addresult);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ LabTask1/LabTask1_Calculator/LabTask1_Calculator/Calculator.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace LabTask1_Calculator
+{
+    class Calculator
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Calculator using C#");
+            Console.WriteLine("Enter num1 == -1 && operand == 'e' && num2 == -1 OR operand == 'e' && num2 == -1 to exit");
+            Console.Write("Please enter the first integer: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            while (true)
+            {
+                Console.Write("Please enter the operand (+, -, /, *, ^): ");
+                char operand = Convert.ToChar(Console.ReadLine());
+
+                Console.Write("Please enter the second integer: ");
+                int num2 = Convert.ToInt32(Console.ReadLine());
+
+                if (operand == '+')
+                {
+                    int add_result = num1 + num2;
+                    Console.WriteLine("{0}", add_result);
+                    num1 = add_result;
+                }
+                else if (operand == '-')
+                {
+                    int sub_result = num1 - num2;
+                    Console.WriteLine("{0}", sub_result);
+                    num1 = sub_result;
+                }
+                else if (operand == '/')
+                {
+                    try
+                    {
+                        int div_result = num1 / num2;
+                        Console.WriteLine("{0}", div_result);
+                        num1 = div_result;
+                    }
+                    catch (DivideByZeroException)
+                    {
+                        Console.WriteLine("can not divided by zero");
+                        Console.WriteLine("Current Output:{0}", num1);
+                    }
+                }
+                else if (operand == '*')
+                {
+                    int mul_result = num1 * num2;
+                    Console.WriteLine("{0}", mul_result);
+                    num1 = mul_result;
+                }
+                else if (operand == '^')
+                {
+                    double pow_res = Math.Pow(num1, num2);
+                    Console.WriteLine("{0}", pow_res);
+                    num1 = (int)pow_res;
+                }
+                else if (num1 == -1 && operand == 'e' && num2 == -1 || operand == 'e' && num2 == -1)
+                {
+                    Console.WriteLine("\nThank you for using C# Calculator.");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Wrong Input.Try Again!");
+                }
+            }
+        }
+    }
+}
